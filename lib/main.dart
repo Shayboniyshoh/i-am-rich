5cbfd105853567b6e759110a34da3ffae0a6bfda
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2022-06-07 19:28:23 +0500

    changed the app icon

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         appBar: AppBar(
           backgroundColor: Colors.lightBlueAccent,
-          title: const Text(
-            'I am rich',
+          title: const Center(
+            child: Text('I am rich'),
           ),
         ),
-        body: const Image(
-            image: NetworkImage(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg')),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/uzcoding_logo.png'),
+          ),
+        ),
       ),
     ),
   );
